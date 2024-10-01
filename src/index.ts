e542aee5973a686b2756e2c14d7dee143137740c
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-10-01 17:10:28 -0500

    feature: morgan

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
+import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
+import morgan from "morgan";
 import { connectDB } from "./db/mongo";
 import dbRoutes from "./routes/db-routes";
 import setupSwagger from "./swagger";
-import bodyParser from "body-parser";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const startApp = async () => {
   app.use(cors());
+  app.use(morgan("dev")); // Para mostrar por consola las peticiones http
   app.use(bodyParser.json());
 
   // Conectar a la base de datos
